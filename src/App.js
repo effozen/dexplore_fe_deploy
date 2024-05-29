09887bada61ae9ce95aee66587b1be4c443fc413
minseoky <minseok184@gmail.com>
2024-05-30 00:33:44 +0900

    feat(WelcomePage.jsx):슬라이드 페이지 프로토타입

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import React from "react";
 import './App.css';
 import ContentsRouter from './components/common/router/ContentsRouter';
+import WelcomePage from "@/components/minseok/WelcomePage";
 
 function App() {
   return (
     <div>
+        <WelcomePage/>
       <ContentsRouter />
     </div>
   );
---
+++ src/components/minseok/WelcomePage.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from "swiper/modules";
+import 'swiper/css';
+import 'swiper/css/pagination';
+import styled from 'styled-components';
+import background from '../../assets/images/museum_BW1.jpg';
+
+const FullScreenWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: url('${background}');
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledSlide = styled(SwiperSlide)`
+  background-color: rgba(0,0,0,100);
+  height: 14vh;
+  border-radius: 10px;
+  text-align: center;
+  align-content: center;
+  padding: 0px 0px 10px 0px;
+  color: #FFFFFF;
+  h3 {
+    font-size: 18px;
+    font-family: Pretendard-Bold;
+  }
+  p {
+    font-size: 15px;
+    font-family: Pretendard-Light;
+  }
+`;
+
+const StyledSwiper = styled(Swiper)`
+  .swiper-pagination-bullet {
+    background-color: #FFFFFF;  //bullet 색상 화이트로 커스텀
+    opacity: 33%;
+  }
+  .swiper-pagination-bullet-active {
+    opacity: 100%;
+  }
+`;
+
+const WelcomePage = () => {
+
+    const inner = [
+        {title:"편리한 관리 시스템", description: "박물관과 작품 정보를 간편하게 관리하세요."},
+        {title:"위치 기반 박물관 탐색", description: "가까운 박물관과 전시품 리스트를 쉽게 확인하고 선택하세요."},
+        {title:"작품 QR코드 자동 생성 및 제공", description: "간편하게 작품 정보를 스캔하여 감상하세요."},
+        {title:"오디오 큐레이팅", description: "작품에 대한 설명과 함께 작품에 몰입하세요."}
+    ]
+
+    return (
+        <FullScreenWrapper>
+            <StyledSwiper
+                modules={[Pagination]}
+                spaceBetween={5}
+                slidesPerView={1}
+                pagination={{ clickable: true }}
+            >
+                {inner.map((item, index) => (
+                    <StyledSlide key={index}>
+                        <h3>{item.title}</h3>
+                        <p>{item.description}</p>
+                    </StyledSlide>
+                ))}
+            </StyledSwiper>
+        </FullScreenWrapper>
+    );
+};
+
+export default WelcomePage;
