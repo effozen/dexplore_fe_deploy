40ea05472ce4a595939b0b2f9f6f71ff5f0c8472
zen <ashgrayblue0@gmail.com>
2024-05-30 13:54:50 +0900

    style: add font
    - add font

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-// import React from "react";
 import './App.css';
 import ContentsRouter from './components/common/router/ContentsRouter';
-import WelcomePage from "@/components/minseok/WelcomePage/WelcomePage";
 
 function App() {
   return (
