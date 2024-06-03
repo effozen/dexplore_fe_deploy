77e651a4aeba6f5fe7f6dc4c713a977649c7456d
zen <ashgrayblue0@gmail.com>
2024-06-03 13:11:53 +0900

    feat(MuseumCreate, MuseumUpdate): 박물관 생성 기능 및 수정기능 구현

--- src/components/admin/museum/create/MuseumCreate.jsx
+++ src/components/admin/museum/create/MuseumCreate.jsx
@@ -1,13 +1,13 @@
 import {Fragment} from "react";
 import Header from "@components/common/frame/Header";
-import MuseumForm from "@components/common/frame/MuseumForm";
+import MuseumCreateForm from "@components/admin/museum/create/MuseumCreateForm";
 
 const MuseumCreate = () => {
 
   return (
     <>
       <Header name='새로운 박물관을 등록하세요' height='100px' isDate={false}></Header>
-      <MuseumForm></MuseumForm>
+      <MuseumCreateForm></MuseumCreateForm>
     </>
   );
 };
