4721ba480a8319fe61b99bbf34567b79d00aa806
minseoky <minseok184@gmail.com>
2024-06-09 18:23:29 +0900

    fix(userMain): 박물관 정보 로딩 최적화 useMemo사용

--- src/components/user/museum/info/MuseumMain.jsx
+++ src/components/user/museum/info/MuseumMain.jsx
@@ -1,27 +1,27 @@
 import ContentCarousel from "@components/common/frame/ContentCarousel";
 import styled from "styled-components";
-import {useEffect, useState} from "react";
-import {requestGet} from "@lib/network/network";
-import {useNavigate} from "react-router-dom";
+import { useEffect, useState, useMemo } from "react";
+import { requestGet } from "@lib/network/network";
+import { useNavigate } from "react-router-dom";
 
 const StyledFrame = styled.div`
-	min-height: 385px;
-	min-width: 375px;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
+  min-height: 385px;
+  min-width: 375px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const StyledDescription = styled.div`
-	font-size: 16px;
-	font-weight: 400;
-	color: #909090;
-	line-height: 18px;
+  font-size: 16px;
+  font-weight: 400;
+  color: #909090;
+  line-height: 18px;
 `;
 
 const StyledFrame2 = styled.div`
-	min-width: 375px;
-	margin-left:auto;
+  min-width: 375px;
+  margin-left: auto;
   margin-top: 20px;
 `;
 
@@ -41,42 +41,48 @@ const StyledButton = styled.button`
   border: white solid 1px;
 `;
 
-const MuseumMain = ({museumInfo = false}) => {
-  const [artList, setArtList] = useState();
+const MuseumMain = ({ museumInfo = false }) => {
+  const [artList, setArtList] = useState([]);
   const navigate = useNavigate();
 
+  const memoizedMuseumInfo = useMemo(() => museumInfo, [museumInfo]);
+
   useEffect(() => {
-    console.log(museumInfo);
-    if (museumInfo) {
-      requestGet('https://dexplore.info/api/v1/user/get-arts', {museumId: museumInfo.museumId}).then((v) => {
+    console.log(memoizedMuseumInfo);
+    if (memoizedMuseumInfo) {
+      requestGet('https://dexplore.info/api/v1/user/get-arts', { museumId: memoizedMuseumInfo.museumId }).then((v) => {
         setArtList(v.artList);
       });
     }
-  }, [museumInfo]);
-
-  useEffect(() => {
-    console.log(artList);
-  }, [artList]);
+  }, [memoizedMuseumInfo]);
 
   const handleClick = () => {
-    navigate('/user/art', {state:{museumInfo}});
-  }
+    navigate('/user/art', { state: { museumInfo: memoizedMuseumInfo } });
+  };
 
   return (
-    <div>
-      <StyledFrame>
-        <img src={museumInfo.imgUrl} alt="" className="w-full min-h-[330px] max-h-[500px] max-w-[800px]"/>
-        <StyledButton onClick={handleClick}>관람 시작하기</StyledButton>
-        <StyledDescription
-          className="w-full max-w-[800px] mt-[20px] pl-[40px] pr-[40px]">
-          {museumInfo.description && (museumInfo.description.length > 500) ? museumInfo.description.substring(0,500) : museumInfo.description}
-        </StyledDescription>
-      </StyledFrame>
-      <StyledFrame2>
-        {artList && <ContentCarousel isAdmin={false} name={`${museumInfo.museumName}의 대표 작품`} isMuseum={false}
-                                     chosenMuseum={museumInfo} itemInfo={artList}></ContentCarousel>}
-      </StyledFrame2>
-    </div>
+      <div>
+        <StyledFrame>
+          <img src={memoizedMuseumInfo.imgUrl} alt="" className="w-full min-h-[330px] max-h-[500px] max-w-[800px]" />
+          <StyledButton onClick={handleClick}>관람 시작하기</StyledButton>
+          <StyledDescription className="w-full max-w-[800px] mt-[20px] pl-[40px] pr-[40px]">
+            {memoizedMuseumInfo.description && memoizedMuseumInfo.description.length > 500
+                ? memoizedMuseumInfo.description.substring(0, 500)
+                : memoizedMuseumInfo.description}
+          </StyledDescription>
+        </StyledFrame>
+        <StyledFrame2>
+          {artList && (
+              <ContentCarousel
+                  isAdmin={false}
+                  name={`${memoizedMuseumInfo.museumName}의 대표 작품`}
+                  isMuseum={false}
+                  chosenMuseum={memoizedMuseumInfo}
+                  itemInfo={artList}
+              />
+          )}
+        </StyledFrame2>
+      </div>
   );
 };
 
