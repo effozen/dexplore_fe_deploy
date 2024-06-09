b89d32afbe7f16cd7abb74202c7c860858f05f43
minseoky <minseok184@gmail.com>
2024-06-09 17:22:22 +0900

    fix(user/main): 북마크 반응형 스타일 추가, 북마크 클릭 시 작품으로 이동, 추천박물관 클릭 시 박물관으로 이동

--- src/components/common/frame/ArtMatrix.jsx
+++ src/components/common/frame/ArtMatrix.jsx
@@ -1,40 +1,95 @@
 import styled from "styled-components";
+import {useNavigate} from "react-router-dom";
+
 // Styled Components
 const StyledFrame = styled.div`
-	margin-left: 16px;
+  margin-left: 16px;
   margin-right: 16px;
-	margin-bottom: 10px;
-	margin-top: 40px;
+  margin-bottom: 10px;
+  margin-top: 40px;
 `;
 
 const StyledHeaderFrame = styled.div`
-	width:100%;
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-  padding-right:20px;
-  margin-right:-20px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-right: 20px;
+  margin-right: -20px;
 `;
 
 const StyledHeader = styled.div`
-	margin-top: 10px;
-	font-weight: 600;
-	color: #000000;
+  margin-top: 10px;
+  font-weight: 600;
+  color: #000000;
 `;
 
-const ArtMatrix = ({title, itemInfo}) => {
-  return (
-      <StyledFrame>
-        <StyledHeaderFrame><StyledHeader>{title}</StyledHeader></StyledHeaderFrame>
-        <div className='grid grid-cols-3 w-[100%] gap-x-[1vw] gap-y-[1vw]'>
-          {itemInfo.map(v => {
-            return (<div className='h-[32vw]'>
-              <img src={v.imgUrl} alt="" className='w-full h-full overflow-hidden' />
-            </div>);
-          })}
-        </div>
-      </StyledFrame>
-  );
-}
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  height: 100%;
+  gap: 2px;
+
+  @media (min-width: 769px) {
+    grid-template-columns: repeat(6, 1fr);
+    gap: 3px;
+  }
+
+  @media (min-width: 1024px) {
+    grid-template-columns: repeat(9, 1fr);
+    gap: 4px;
+  }
+
+  div {
+    position: relative;
+    width: 100%;
+    padding-bottom: 100%; /* This makes the div a square */
+  }
+
+  img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover; /* This makes the image cover the square */
+  }
+`;
+
+const LoadingContainer = styled.div`
+  height: 200px;
+  width: 100%;
+  text-align: center;
+  align-content: center;
+  font-size: 20px;
+  font-weight: 600;
+`
+
+const ArtMatrix = ({ title, itemInfo }) => {
+
+    const navigate = useNavigate();
+
+    const handleClick = (id) => {
+        navigate("/user/art/info/", { state: { artId: id } });
+    };
+
+    return (
+        <StyledFrame>
+            <StyledHeaderFrame>
+                <StyledHeader>{title}</StyledHeader>
+            </StyledHeaderFrame>
+            {itemInfo.length === 0 ? <LoadingContainer>로딩 중...</LoadingContainer>:
+                <GridContainer>
+                    {itemInfo.map((v, index) => (
+                        <div key={index} onClick={() => handleClick(v.artId)}>
+                            <img src={v.imgUrl} alt={`Artwork ${index}`} />
+                        </div>
+                    ))}
+                </GridContainer>
+            }
+
+        </StyledFrame>
+    );
+};
 
 export default ArtMatrix;
